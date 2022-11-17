6beaba7bed715a7a5aeb93924c69e197927de943
Marieme Doua <marieme.doua@gmail.com>
2022-11-17 14:47:17 -0500

    some optimisation

--- js/script.js
+++ js/script.js
@@ -4,10 +4,9 @@ class MShift {
 }
 
 let ShiftMentorList = [];
-let x=0
 
 
-window.onloadstart= function () {
+window.onload= function () {
    
     initializeShifts();
 
@@ -15,53 +14,37 @@ window.onloadstart= function () {
 }   
 
 
-
-
 function initializeShifts() {
     
     let la1 = new MShift();
     la1.MentorName = "Mentor1";
     la1.MentorShifts = ["shift1", "shift2", "shift3"];
-     
-    ShiftMentorList[0] = la1;
     
     let la2 = new MShift();
     la2.MentorName = "Mentor2";
     la2.MentorShifts = ["shift1", "shift2"];
 
-    ShiftMentorList[1] = la2;
-
     let la3 = new MShift();
     la3.MentorName = "Mentor3";
     la3.MentorShifts = ["shift1", "shift2", "shift3", "shift4", "shift5"];
-
-    ShiftMentorList[2] = la3;
 
     let la4 = new MShift();
     la4.MentorName = "Mentor4";
     la4.MentorShifts = ["shift1", "shift2", "shift3", "shift4", "shift5"];
 
-    ShiftMentorList[3] = la4;
-
     let la5 = new MShift();
     la5.MentorName = "Mentor5";
     la5.MentorShifts = ["shift1", "shift2", "shift3", "shift4"];
-
-    ShiftMentorList[4] = la5;
 
     let la6 = new MShift();
     la6.MentorName = "Mentor6";
     la6.MentorShifts = ["shift1", "shift2", "shift3", "shift4"];
 
-    ShiftMentorList[5] = la6;
+    ShiftMentorList =[la1,la2,la3,la4,la5,la6];
 }
 
-function bindMentorsToDropDown() {
-    
+function bindMentorsToDropDown() {  
     let dropDown = document.getElementById("ments");
-
-    dropDown.options.length = 0;
-
     let el = document.createElement("option");
     el.textContent = "Select a mentor...";
     dropDown.appendChild(el);
@@ -113,42 +96,32 @@ function myFunction2(){
     navbar1.classList.toggle('show');
 }
 
-function getFName(form){
-    return form[2].value;
-}
-
-function getLName(form){
-    return form[3].value;
-}
-
-function getEmail(form){
-    return form[5].value;
-}
-
 function ValidateEmail(mail) {
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    
     return mail.match(mailformat);
 }
 
 function myFun(form){
-   let fname= "";
-   let lname= ""; 
-   let email= "";
-   fname = getFName(form);
-   lname=  getLName(form);
-   email= getEmail(form);
-  
+   let mentorSc = form[0].value;
+   let shiftSc = form[1].value;
+   let fname = form[2].value;
+   let lname = form[3].value; 
+   let phone = form[4].value;
+   let email = form[5].value;
+   let message = form[6].value;
+
    const c1 = "Thank you " ;
    const c2 = " for your appointment.";
    
    let correctmail = ValidateEmail(email);
 
    if(correctmail){
+
         let message = c1 + fname + " " + lname + c2;
-        document.getElementById("thanks").innerHTML = message;
+        document.getElementById("confMsg").innerHTML = message;
     }
     else {  
-        document.getElementById("thanks").innerHTML = "Please correct your email";
+        document.getElementById("confMsg").innerHTML = "Please correct your email";
     }
 }
 
